b2b15002bfbbbe508b18eb72bf55ca39d30da651
Mark Donohue <markbdonohue@gmail.com>
2023-07-23 15:11:28 -0400

    Flesh out navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { Link } from "react-scroll/modules";
+import { IoMenu, IoClose } from "react-icons/io5";
+
+interface NavItem {
+  label: string;
+  section: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: "Home",
+    section: "home",
+  },
+  {
+    label: "About",
+    section: "about",
+  },
+  {
+    label: "Experience",
+    section: "experience",
+  },
+];
 
 const Navigation = () => {
-  return <div></div>;
+  const [sideMenu, setSideMenu] = useState(false);
+
+  return (
+    <header className="w-full mx-auto px-4 sm:px-10 bg-[#F8F8F7] shadow fixed top-0 z-50">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between py-2 md:block">
+            <div className="md:py-5 md:block">
+              <h2 className="text-2xl font-bold">Mark Donohue</h2>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => setSideMenu(!sideMenu)}>
+                {sideMenu ? <IoClose size={36} /> : <IoMenu size={36} />}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              sideMenu ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((navItem, idx) => {
+                return (
+                  <Link
+                    className="block lg:inline-block uppercase hover:text-[#167BFF]"
+                    key={idx}
+                    to={navItem.section}
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setSideMenu(!sideMenu)}
+                  >{navItem.label}</Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
