95a4c07e55c5e4ab75947a0712902643d5d67fe3
Mark Donohue <markbdonohue@gmail.com>
2023-07-23 15:18:01 -0400

    Adjust navbar icon padding

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -28,14 +28,14 @@ const Navigation = () => {
   const [sideMenu, setSideMenu] = useState(false);
 
   return (
-    <header className="w-full mx-auto px-4 sm:px-10 bg-[#F8F8F7] shadow fixed top-0 z-50">
+    <header className="w-full mx-auto px-4 sm:px-8 bg-[#F8F8F7] shadow fixed top-0 z-50">
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-2 md:block">
             <div className="md:py-5 md:block">
               <h2 className="text-2xl font-bold">Mark Donohue</h2>
             </div>
-            <div className="md:hidden">
+            <div className="md:hidden pt-3">
               <button onClick={() => setSideMenu(!sideMenu)}>
                 {sideMenu ? <IoClose size={36} /> : <IoMenu size={36} />}
               </button>
@@ -45,11 +45,11 @@ const Navigation = () => {
 
         <div>
           <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            className={`flex-1 justify-self-center pb-3 mt-4 md:block md:pb-0 md:mt-0 ${
               sideMenu ? "block" : "hidden"
             }`}
           >
-            <div className="items-center justify-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+            <div className="items-center justify-center space-y-4 md:flex md:space-x-6 md:space-y-0">
               {NAV_ITEMS.map((navItem, idx) => {
                 return (
                   <Link
