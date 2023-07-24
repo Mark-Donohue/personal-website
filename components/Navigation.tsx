7050d336c09b27542e77a819d25f2ea4590bace7
Mark Donohue <markbdonohue@gmail.com>
2023-07-23 21:36:41 -0400

    Tweaks to navbar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -9,6 +9,7 @@ interface NavItem {
   section: string;
 }
 
+// Menu Options
 const NAV_ITEMS: Array<NavItem> = [
   {
     label: "Home",
@@ -22,40 +23,42 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "Experience",
     section: "experience",
   },
+  {
+    label: "Résumé",
+    section: "Résumé"
+  }
 ];
 
 const Navigation = () => {
-  const [sideMenu, setSideMenu] = useState(false);
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
 
   return (
     <header className="w-full mx-auto px-4 sm:px-8 bg-[#F8F8F7] shadow fixed top-0 z-50">
       <div className="justify-between md:items-center md:flex">
-        {/* Logo and Side Menu */}
         <div>
           <div className="flex items-center justify-between py-2 md:block">
             <div className="md:py-5 md:block">
               <h2 className="text-2xl font-bold">Mark Donohue</h2>
             </div>
             <div className="md:hidden pt-3">
-              <button onClick={() => setSideMenu(!sideMenu)}>
-                {sideMenu ? <IoClose size={36} /> : <IoMenu size={36} />}
+              <button onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}>
+                {isSideMenuOpen ? <IoClose size={36} /> : <IoMenu size={36} />}
               </button>
             </div>
           </div>
         </div>
 
-        {/* Menu Links */}
         <div>
           <div
             className={`flex-1 justify-self-center pb-3 mt-4 md:block md:pb-0 md:mt-0 ${
-              sideMenu ? "block" : "hidden"
+              isSideMenuOpen ? "block" : "hidden"
             }`}
           >
             <div className="items-center justify-center space-y-4 md:flex md:space-x-6 md:space-y-0">
               {NAV_ITEMS.map((navItem, idx) => {
                 return (
                   <Link
-                    className="block lg:inline-block uppercase hover:text-[#167BFF]"
+                    className="block lg:inline-block uppercase hover:cursor-pointer hover:text-[#167BFF]"
                     key={idx}
                     to={navItem.section}
                     activeClass="active"
@@ -63,7 +66,7 @@ const Navigation = () => {
                     smooth={true}
                     offset={-100}
                     duration={500}
-                    onClick={() => setSideMenu(!sideMenu)}
+                    onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}
                   >
                     {navItem.label}
                   </Link>
