a26fa76db0cfc015ba315efed99f4242f91276d6
Mark Donohue <markbdonohue@gmail.com>
2023-07-25 08:50:09 -0400

    Style hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,12 +7,21 @@ import { Link } from "react-scroll/modules";
 const Hero = () => {
   return (
     <section id="home">
-      <div>
+      <div className="flex flex-col text-center items-center justify-center my-10 py-16">
         <div>
-          <h1>
+          <Image
+            className="rounded-full"
+            src="/headshot.jpg"
+            alt=""
+            width={300}
+            height={300}
+          />
+        </div>
+        <div>
+          <h1 className="font-bold text-4xl mt-6">
             Hi, I&apos;m <span className="text-[#167BFF]">Mark</span>
           </h1>
-          <p>
+          <p className="text-lg mt-4 mb-6">
             I&apos;m a Software Engineer based in New York City. Currently,
             I&apos;m focused on designing and building efficient and secure APIs
             at{" "}
@@ -26,8 +35,8 @@ const Hero = () => {
             </a>
           </p>
           <Link
-            to="resume"
-            className="text-neutral-100 uppercase font-semibold px-6 py-3 bg-[#167BFF]"
+            to=""
+            className="rounded-md text-neutral-100 uppercase font-semibold px-6 py-3 shadow bg-[#167BFF] hover:bg-[#136EE5]"
             activeClass="active"
             spy={true}
             smooth={true}
@@ -36,9 +45,6 @@ const Hero = () => {
           >
             GET IN TOUCH
           </Link>
-        </div>
-        <div>
-          <Image src="/headshot.jpg" alt="" width={300} height={300} />
         </div>
       </div>
     </section>
